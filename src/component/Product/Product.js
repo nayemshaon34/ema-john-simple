f9150ccb4549f751e15abd5efef7336ce47e8d86
nayemshaon34 <nayemshaon34@gmail.com>
2021-09-26 02:45:39 +0600

    button added

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -2,14 +2,19 @@ import './Product.css'
 import React from 'react';
 
 const Product = (props) => {
+    const {img,seller,name,price,stock} = props.product;
     console.log(props.product);
     return (
         <div className="product">
             <div className="proudct-image">
-                    <img src={props.product.img} alt="" srcset="" />
+                    <img src={img} alt="" srcset="" />
             </div>
             <div className="product-des">
-                    <h2>{props.product.name}</h2>
+                    <h4>{name}</h4>
+                    <p><small>by: {seller}</small></p>
+                    <h3>${price}</h3>
+                    <p><small>only {stock} left in stock - order soon</small></p>
+                    <button onClick="hadnleEvent" className="add-btn">add to cart</button>
             </div>
         </div>
     );
